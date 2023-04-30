5f845b752ac6bcc6811204f94dba36b35efc108f
Marlon Angeli <marlonangeli91@gmail.com>
2023-04-30 03:21:49 -0300

    fix: fix CPF validation

--- src/Healthy.Domain/Validators/Validation.cs
+++ src/Healthy.Domain/Validators/Validation.cs
@@ -7,36 +7,45 @@ internal static class Validation
     /// </summary>
     /// <param name="cpf"></param>
     /// <returns></returns>
-    public static bool ValidateCPF(string? cpf)
+    public static bool ValidateCPF(string cpf)
     {
         if (string.IsNullOrEmpty(cpf))
+        {
             return false;
+        }
 
         cpf = cpf.RemoveMask();
 
-        if (cpf!.Length != 11 || !cpf.All(char.IsDigit))
+        if (cpf.Length != 11 || !cpf.All(char.IsDigit))
+        {
             return false;
+        }
 
-        if (new string(cpf[0], cpf.Length) == cpf)
+        if (cpf.Distinct().Count() == 1)
+        {
             return false;
+        }
 
         int[] weights1 = { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
         int[] weights2 = { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
 
-        var digits = cpf.Select(c => int.Parse(c.ToString())).ToArray();
+        int[] digits = cpf.Select(c => c - '0').ToArray();
 
-        int sum1 = 0, sum2 = 0;
-        for (int i = 0; i < 9; i++)
-        {
-            sum1 += digits[i] * weights1[i];
-            sum2 += digits[i] * weights2[i];
-        }
+        int sum1 = digits.Take(9).Zip(weights1, (a, b) => a * b).Sum();
+        int sum2 = digits.Take(10).Zip(weights2, (a, b) => a * b).Sum();
 
-        int remainder1 = sum1 % 11, remainder2 = sum2 % 11;
-        if (remainder1 < 2) remainder1 = 0;
-        else remainder1 = 11 - remainder1;
-        if (remainder2 < 2) remainder2 = 0;
-        else remainder2 = 11 - remainder2;
+        int remainder1 = sum1 % 11;
+        int remainder2 = sum2 % 11;
+        
+        if (remainder1 < 2)
+            remainder1 = 0;
+        else
+            remainder1 = 11 - remainder1;
+        
+        if (remainder2 < 2)
+            remainder2 = 0;
+        else
+            remainder2 = 11 - remainder2;
 
         return digits[9] == remainder1 && digits[10] == remainder2;
     }
