c522fe5b6400fe1d4ccad2e5ac27021bd1aaacb5
Marlon Angeli <marlonangeli91@gmail.com>
2023-05-03 11:40:23 -0300

    feat: add form masks

--- src/Healthy.Web/wwwroot/js/site.js
+++ src/Healthy.Web/wwwroot/js/site.js
@@ -2,3 +2,57 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// Doctor
+$(document).ready(function () {
+    $('.datepicker').datepicker({
+        format: 'dd/mm/yyyy',
+        language: 'pt-BR',
+        autoclose: true,
+        todayHighlight: true
+    });
+
+    $('.crm').mask('0000000');
+    $('.phone').mask('(00) 00000-0000');
+    $('.email').mask('A', {
+        translation: {
+            'A': {pattern: "/[\w@@\-.+]/", recursive: true}
+        }
+    });
+});
+
+// Appointment
+$(document).ready(function () {
+    $('.datepicker').datepicker({
+        format: 'dd/mm/yyyy',
+        language: 'pt-BR',
+        autoclose: true,
+        todayHighlight: true
+    });
+
+    $('.invoice').maskMoney({
+        prefix: 'R$ ',
+        allowNegative: false,
+        thousands: '.',
+        decimal: ',',
+        affixesStay: false
+    });
+});
+
+// Patient
+$(document).ready(function () {
+    $('.datepicker').datepicker({
+        format: 'dd/mm/yyyy',
+        language: 'pt-BR',
+        autoclose: true,
+        todayHighlight: true
+    });
+
+    $('.cpf').mask('000.000.000-00', { reverse: true });
+    $('.phone').mask('(00) 00000-0000');
+    $('.email').mask('A', {
+        translation: {
+            'A': { pattern: "/[\w@@\-.+]/", recursive: true }
+        }
+    });
+});
